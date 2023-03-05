b9c1a32051ec9d52f3cb24c7c3689e16f98e904d
emekarr <aceanyanwu02@gmail.com>
2023-03-05 14:30:24 +0100

    built main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"github.com/emekarr/coding-test-busha/logger"
+	"github.com/emekarr/coding-test-busha/server_response"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("could not load env variables")
+	}
+
+	server := gin.Default()
+
+	server.Use(cors.Default())
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		server_response.Respond(ctx, http.StatusOK, "server is up and running", true, nil)
+	})
+
+	server.NoRoute(func(ctx *gin.Context) {
+		server_response.Respond(ctx, http.StatusNotFound, "this route does not exist", false, nil)
+	})
+
+	gin_mode := os.Getenv("GIN_MODE")
+	port := os.Getenv("PORT")
+	if gin_mode == "debug" {
+		server.Run(port)
+	} else if gin_mode == "release" {
+		server.Run(":" + port)
+	} else {
+		panic("invalid gin mode used")
+	}
+	logger.Info("server is up on port" + port)
+}
