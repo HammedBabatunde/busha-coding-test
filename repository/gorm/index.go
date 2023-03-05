d40e4aa32024c9bc32e9a03e84d2f4c0430afdef
emekarr <aceanyanwu02@gmail.com>
2023-03-05 14:33:01 +0100

    built db repository

---
+++ repository/gorm/index.go
@@ -0,0 +1,40 @@
+package gorm
+
+import (
+	"errors"
+
+	"github.com/emekarr/coding-test-busha/logger"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type GormRepository[T interface{}] struct {
+	Gorm *gorm.DB
+}
+
+func (gr *GormRepository[T]) errFilter(err error, filter error) error {
+	if errors.Is(err, filter) {
+		return nil
+	}
+	return err
+}
+
+func (gr *GormRepository[T]) CreateOne(payload *T) (*T, error) {
+	err := gr.Gorm.Create(payload).Error
+	if err != nil {
+		logger.Error(errors.New("db error - could not insert record"), zap.Error(err))
+		return nil, err
+	}
+	return payload, nil
+}
+
+// Parameter filter can be of type string or struct pointer
+func (gr *GormRepository[T]) FindMany(filter interface{}) (*[]T, error) {
+	payload := []T{}
+	result := gr.Gorm.Where(filter).Find(payload)
+	if err := gr.errFilter(result.Error, gorm.ErrRecordNotFound); err != nil {
+		logger.Error(errors.New("db error - find many search failed"), zap.Error(result.Error))
+		return nil, err
+	}
+	return &payload, nil
+}
