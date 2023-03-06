5679c402c1bd42a4d84481b41c5b35a8c7c80b21
emekarr <aceanyanwu02@gmail.com>
2023-03-06 08:16:43 +0100

    init comments repo using sync.once

--- app/comment/repository/index.go
+++ app/comment/repository/index.go
@@ -1,9 +1,20 @@
 package repository
 
 import (
+	"sync"
+
 	"github.com/emekarr/coding-test-busha/app/comment/models"
 	"github.com/emekarr/coding-test-busha/db"
 	gormRepo "github.com/emekarr/coding-test-busha/repository/gorm"
 )
 
-var CommentRepository = gormRepo.GormRepository[models.Comment]{Gorm: db.GormDB}
+var once = &sync.Once{}
+
+var CommentRepository gormRepo.GormRepository[models.Comment]
+
+func GetCommentRepository() gormRepo.GormRepository[models.Comment] {
+	once.Do(func() {
+		CommentRepository = gormRepo.GormRepository[models.Comment]{Gorm: db.GormDB}
+	})
+	return CommentRepository
+}
